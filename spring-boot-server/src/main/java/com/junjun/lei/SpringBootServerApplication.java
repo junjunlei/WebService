d44662397b3b4ee3aff11c527dc91c4d8a9c9270
雷俊俊 <junjun.lei@hand-china.com>
2020-08-14 15:01:23 +0800

    [ADD]:初始化项目

---
+++ spring-boot-server/src/main/java/com/junjun/lei/SpringBootServerApplication.java
@@ -0,0 +1,16 @@
+package com.junjun.lei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Junjunlei
+ */
+@SpringBootApplication
+public class SpringBootServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootServerApplication.class, args);
+    }
+
+}
